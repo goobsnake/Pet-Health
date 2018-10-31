9b801d70292aff203cfd5e5429331d7bfa9aab86
goobsnake <goobsnake@gmail.com>
2018-10-31 10:31:55 -0700

    Updated to main LUA file

--- PetHealth.lua
+++ PetHealth.lua
@@ -1,3 +1,6 @@
+--Elder Scrolls: Online addon (written in LUA) which adds persistent in-game health bars to all permanent pets. 
+--Original/base work of this addon was developed by SCOOTWORKS and I was granted permission by him to take over full development and distribution of this addon.
+
 local addon = {
 	name = "PetHealth",
 	displayName = "PetHealth",
